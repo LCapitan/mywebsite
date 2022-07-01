0811720cf46a3c85a612c721cc5dcbd3e4d7b17d
Austin Melendez <AustinJames17@icloud.com>
2022-07-01 07:39:36 -0700

    fix mobile issues

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 import { Button } from '../../components';
-import { HomeBg } from '../../components/Icons/';
+import { HomeBg, Logo } from '../../components/Icons/';
 
 import styles from './Home.module.scss'
 
@@ -14,6 +14,9 @@ export function Home() {
   return (
     <div className={styles.home}>
       <div className={styles.content}>
+        <div className={styles.logo}>
+          <Logo />
+        </div>
         <h1 className={styles.title}>austin<br />melendez</h1>
         <h2 className={styles.subtitle}>artist, designer, and developer from Richmond, VA.</h2>
         <div className={styles.actions}>
@@ -24,13 +27,13 @@ export function Home() {
       <div className={styles.pageBg}>
         <HomeBg />
       </div>
-      <figure className={styles.pageHero}>
+      <div className={styles.pageHero}>
         <Image src="https://res.cloudinary.com/austinmel/image/upload/v1656674282/austin-computer_m9bpcz.png"
           layout="responsive"
           alt="an illustration of me using the computer"
           width="1604"
           height="1515" />
-      </figure>
+      </div>
     </div>
   );
 }
