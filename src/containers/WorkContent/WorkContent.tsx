c8eea539ab4228b78128fca8b9d9bca34a3d55b5
Austin Melendez <AustinJames17@icloud.com>
2022-07-02 02:22:15 -0400

    add more styles for desktop and mobile and change menu color

--- src/containers/WorkContent/WorkContent.tsx
+++ src/containers/WorkContent/WorkContent.tsx
@@ -3,12 +3,16 @@ import Link from "next/link";
 import Image from "next/image";
 
 import { Button, WorkCard } from "../../components";
+import { HomeBg } from '../../components/Icons/';
 
 import styles from './WorkContent.module.scss';
 
 const WorkContent = () => {
   return (
     <div className={styles.work}>
+      <div className={styles.pageBg}>
+        <HomeBg />
+      </div>
       <div className={styles.pageHeading}>
         <h1 className={styles.pageTitle}>work</h1>
         <h2 className={styles.subtitle}>Here are a few of the most recent sites I&#39;ve worked on or built. (Feel free to reach out to view other various projects).</h2>
@@ -122,6 +126,10 @@ const WorkContent = () => {
           />
         </li>
       </ul>
+
+      <div className={styles.actions}>
+        <Button url="mailto:austinjamesmelendez@gmail.com" label="let's chat" />
+      </div>
     </div>
   );
 }
