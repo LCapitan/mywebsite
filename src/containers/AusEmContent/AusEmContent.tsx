f7d65f663249c1285524138e802df08da510e3c6
Austin Melendez <AustinJames17@icloud.com>
2022-11-18 23:32:23 -0500

    update content

--- src/containers/AusEmContent/AusEmContent.tsx
+++ src/containers/AusEmContent/AusEmContent.tsx
@@ -16,51 +16,50 @@ const AusEmContent = () => {
             <p>
               Honestly, no girl has ever made me laugh so hard, so much. We got
               along so well and so quickly because we&#39;re the same in so many
-              - almost too many - ways. But we took these similarities in our
-              lives as a measure of compatibility and you were right, we&#39;re
-              probably not compatible. We both know God brought us together when
-              He did for a reason, and I think we both know now that it
-              wasn&#39;t to be together romantically. To be honest with you..He
-              showed me this after I moved, which is why I&#39;d mentioned a few
-              times that it seemed like you just needed someone to be there for
-              you so maybe we shouldn&#39;t be dating and just be friends. I
-              should&#39;ve told you that I felt God was telling me that, I
-              should&#39;ve said let&#39;s pray about it. Instead I just let
-              things go on and ignored God. Which is probably why we ended up
-              the way we did. We both hurt each other, for sure, but I
-              deliberately disobeyed God - multiple times - and if I hadn&#39;t
-              none of this would have happened.
+              - almost too many - ways. We both know God brought us together
+              when He did for a reason, and I thought it was for us to be
+              together. He showed me after I moved, that maybe that wasn&#39;t
+              the reason..which is why I&#39;d mentioned a few times that it
+              seemed like you just needed someone to be there for you so maybe
+              we shouldn&#39;t be dating and just be friends. I should&#39;ve
+              told you that I felt God was telling me that, I should&#39;ve said
+              let&#39;s pray about it. Instead I just let things go on and
+              ignored God. Which is why we ended up the way we did. We both hurt
+              each other, sure, but I deliberately disobeyed God - multiple
+              times - and if I hadn&#39;t none of this would have happened. I
+              was supposed to protect you and your heart...and I didn&#39;t.
             </p>
             <p>
-              I broke down so badly yesterday bc I&#39;m realizing my own crappy
-              patterns. I&#39;m realizing how many times you were right about
-              me, and how many times I shrugged you off. How I head into
+              I broke down so badly yesterday because I&#39;m realizing my own
+              bad patterns. I&#39;m realizing how many times you were right
+              about me, and how many times I shrugged you off. How I head into
               darkness whenever I don&#39;t want to do something God is telling
               me to. I&#39;m realizing that, yeah we perceive things
-              differently, but that&#39;s bc I only view things from my
+              differently, but that&#39;s because I only view things from my
               perspective. We both could have been better, but as a man I should
               have done what was right for us and I didn&#39;t and it snowballed
               into a disastrous chapter for both of our lives.
             </p>
             <p>
-              You kept saying I need to give things to God, and I have been. You
+              You&#39;ve said I need to give things to God, and I have been. You
               said I have to want to get better and I do. Every time I give
               things to God, it&#39;s like he reveals a memory that I&#39;d
-              forgotten where I handled the wrong way, or where you told me
-              something and I ignored it. And it sucks. In therapy it&#39;s been
-              a nightmare to face myself and learn all these things, especially
-              when so many of the things were things you told me about myself
-              and I completely dismissed your opinion.
+              forgotten where I handled things the wrong way, or where you told
+              me something and I ignored it. And it sucks. In therapy it&#39;s
+              been a nightmare to face myself and learn all these things,
+              especially when so many of the things were things you told me
+              about myself and I completely dismissed your opinion.
             </p>
             <p>
               I am really sorry that I&#39;ve been dragging you through such a
               miserable break up too. You did what you needed to be happy and I
-              made you feel bad bc it meant not being with me and I couldn&#39;t
-              understand how destructive I was/have been. Everyday for more than
-              a year I&#39;ve just been planning this future with you that&#39;s
-              gone and the real problem is that I wasn&#39;t present enough to
-              do what had to be done and be who I needed to be for you to make
-              that future possible. For that I truly apologize.
+              made you feel bad because it meant not being with me and I
+              couldn&#39;t understand how destructive and mean I was/have been.
+              Everyday for more than a year I&#39;ve just been planning this
+              future with you that&#39;s evaporated and the real problem is that
+              I wasn&#39;t present enough to do what had to be done and be who I
+              needed to be for you to make that future possible. For that I
+              truly apologize.
             </p>
             <p>
               I am going to let you go, I promise. I&#39;m just seeing things a
@@ -86,19 +85,20 @@ const AusEmContent = () => {
               about it and yeah you were right, I did kind of just want you to
               submit and do things. That was wrong of me. You were right about
               my acts of love being transactional. I was just going through the
-              motions, and then expected you to do what I wanted to. And so many
-              other things. I should have listened to you and I wish I did.
+              motions, and then expected you to do what I wanted you to. And so
+              many other things. I should have listened to you and I wish I did.
             </p>
             <p>
               You didn&#39;t make me go crazy, I&#39;ve just spent so much time
               in denial and have avoided my own growth and rejected the help
-              you&#39;d consistently offered and desperately encouraged and I am
-              painfully, and regretfully sorry.
+              you&#39;d consistently and so lovingly offer me and desperately
+              encouraged and I am painfully, and regretfully sorry that I
+              wouldn&#39;t listen to you.
             </p>
             <p>
-              I know I have a tough road ahead bc God is just going to show me
-              more. I won&#39;t keep reaching out to you but, know you are not
-              who I made you out to be and. We both sort of became what we
+              I know I have a tough road ahead because God is just going to show
+              me more. I won&#39;t keep reaching out to you but, know you are
+              not who I made you out to be and. We both sort of became what we
               feared most about each other. Be it trauma, baggage, ptsd, etc, we
               should have been better and I should have led that and I&#39;m
               sorry. This morning was the first time in a long time that I
@@ -122,40 +122,42 @@ const AusEmContent = () => {
               you&#39;re supportive and encouraging, you&#39;re considerate,
               you&#39;re wildly forgiving and graceful, you&#39;re smooth aslll,
               you&#39;re ambitious, you&#39;re thoughtful, you&#39;re loved, and
-              you are worthy and deserving of the WORLD, Emily.
+              you are worthy and deserving of the world, Emily. I wish I could
+              have given it to you.
             </p>
             <p>
               I&#39;m sorry I won&#39;t be around to watch you thrive and
-              dominate the XD world, but I&#39;m so proud of you. You&#39;re
-              going to do amazing things in this world and I have no doubt that
-              everyone you meet from here on out will look up to you and want to
-              be like you. Your life is going to be much better without me, and
-              I&#39;m really excited to hear about everything you accomplish in
-              your life if we ever run into each other again.
+              dominate the XD world, but I&#39;m so, so proud of you. You&#39;re
+              going to do amazing things in this lifetime and I have no doubt
+              that everyone you meet from here on out will look up to you and
+              want to be like you. Your life is going to be much better without
+              me, and I&#39;m really excited to hear about everything you
+              accomplish in your life if we ever run into each other again.
             </p>
             <p>
-              And yeah, this has felt like one long, horribly bad dream, but I
-              am finally waking up..
+              And yeah, this has felt like a bad dream, but it&#39;s because of
+              me. But I am finally waking up..
             </p>
             <p>
-              As the great mistress of Micah once said, I&#39;m not sure how to
-              land this plane, but I hope you grow and learn to love yourself
+              As Micah&#39;s bitch of a mistress once said, I&#39;m not sure how
+              to land this plane, but I hope you grow and learn to love yourself
               and see you the way that I see you so you don&#39;t get stuck with
               someone like me again. I hope you have an amazing life, a loving
-              husband, a beautiful family, and I hope God reveals your purpose
-              to you soon and continues to fill your heart.
+              and nurturing husband, a beautiful family, and I hope God reveals
+              your purpose to you soon and continues to fill your heart.
             </p>
             <p>
-              From the bottom of my poorly functioning heart, I&#39;m sorry
-              about that wedge and I miss you so much..
+              From the bottom of my broken heart, I&#39;m sorry about that wedge
+              and I miss you so much..
             </p>
             <p className={styles.sign}>I will never stop loving you,</p>
             <p>Austeoporosis</p>
             <p className={styles.sign}>
-              ps: I&#39;m still down for the 32 & single deal we made if you are
-              so, just let me know. I&#39;ll have the farm and venue business
-              ready for you..
+              ps: I&#39;m still down for the 32 & single deal we made if you
+              are. I&#39;ll have the farm, the chickens, goats, buns, and
+              Mantequilla, plsu the wedding venue business ready for you..
             </p>
+            <p>thank you for loving me, Emily...</p>
           </div>
         </div>
 
