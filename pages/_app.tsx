ec14449b58b016e1495819a06115031d9fe52d72
Austin Melendez <AustinJames17@icloud.com>
2022-11-18 23:56:35 -0500

    fix build error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,34 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
-import UIContextProvider from '../src/providers/UIContextProvider'
-import { Header, Footer } from '../src/components'
-import { DefaultSeo } from 'next-seo';
-import Head from 'next/head'
-
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import UIContextProvider from "../src/providers/UIContextProvider";
+import { Header } from "../src/components";
+import { DefaultSeo } from "next-seo";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UIContextProvider>
       <>
         <Head>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
           <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
         <DefaultSeo
           openGraph={{
-            url: 'https://www.amelen.dev/',
+            url: "https://www.amelen.dev/",
             title: "Austin's Portfolio",
             description: "Austin is a developer and designer, Virginia, US.",
             images: [
               {
-                url: 'https://res.cloudinary.com/austinmel/image/upload/v1656678415/astro_iie61u.jpg',
+                url: "https://res.cloudinary.com/austinmel/image/upload/v1656678415/astro_iie61u.jpg",
                 width: 1440,
                 height: 1440,
-                alt: 'astronaut illustration',
-                type: 'image/jpeg',
-              }
+                alt: "astronaut illustration",
+                type: "image/jpeg",
+              },
             ],
           }}
         />
@@ -35,7 +37,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         {/* <Footer /> */}
       </>
     </UIContextProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
